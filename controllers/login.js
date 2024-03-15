6b578ae03cca62d3d43d43acded0ce79ca2a6863
SkanderManai <skander.manai23@gmail.com>
2024-03-15 17:52:41 +0100

    Part 5-9

--- controllers/login.js
+++ controllers/login.js
@@ -4,32 +4,34 @@ const loginRouter = require("express").Router();
 const User = require("../models/user");
 
 loginRouter.post("/", async (request, response) => {
-  const { username, password } = request.body;
+    const { username, password } = request.body;
 
-  const user = await User.findOne({ username });
-  // console.log(user);
-  const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash);
-  // console.log(passwordCorrect);
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: "invalid username or password",
+    const user = await User.findOne({ username });
+    // console.log(user);
+    const passwordCorrect =
+        user === null
+            ? false
+            : await bcrypt.compare(password, user.passwordHash);
+    // console.log(passwordCorrect);
+    if (!(user && passwordCorrect)) {
+        return response.status(401).json({
+            error: "invalid username or password",
+        });
+    }
+
+    const userForToken = {
+        username: user.username,
+        id: user._id,
+    };
+
+    // token expires in one hour
+    const token = jwt.sign(userForToken, process.env.SECRET, {
+        expiresIn: 60 * 60,
     });
-  }
 
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  };
-
-  // token expires in one hour
-  const token = jwt.sign(userForToken, process.env.SECRET, {
-    expiresIn: 60 * 60,
-  });
-
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name });
+    response
+        .status(200)
+        .send({ token, username: user.username, name: user.name });
 });
 
 module.exports = loginRouter;
--- controllers/users.js
+++ controllers/users.js
@@ -4,32 +4,32 @@ const User = require("../models/user");
 const passwordValidator = require("../utils/passwordValidator");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({}).populate("notes", {
-    content: 1,
-    important: 1,
-  });
-  response.json(users);
+    const users = await User.find({}).populate("notes", {
+        content: 1,
+        important: 1,
+    });
+    response.json(users);
 });
 
 usersRouter.post("/", async (request, response) => {
-  const { username, name, password } = request.body;
+    const { username, name, password } = request.body;
 
-  if (!passwordValidator(password)) {
-    return response.status(400).json({ error: "password too weak" });
-  }
+    if (!passwordValidator(password)) {
+        return response.status(400).json({ error: "password too weak" });
+    }
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    });
 
-  const savedUser = await user.save();
+    const savedUser = await user.save();
 
-  response.status(201).json(savedUser);
+    response.status(201).json(savedUser);
 });
 
 module.exports = usersRouter;
